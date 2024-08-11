569fc6600150997c3ec85660e3a3b518191a4b12
Krishna Kaushik <58665932+tkkaushik369@users.noreply.github.com>
2024-08-11 16:27:02 +0530

    render site config

--- src/webpack.dev.js
+++ src/webpack.dev.js
@@ -12,7 +12,7 @@ module.exports = merge(common, {
 		hot: true,
 		proxy: [{
 			context: "/socket.io",
-			target: "http://127.0.0.1:3000",
+			target: "https://socketcontrol.onrender.com",
 			ws: true,
 		},],
 	},
